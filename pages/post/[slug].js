f2642dfedde77108e6cf4b1f58341e48f13d15a7
akhil chaudhary <chaudharyakhil337@gmail.com>
2021-11-16 22:17:07 +0530

    commit

---
+++ pages/post/[slug].js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Categories, PostDetail, PostWidget, Author, Comments, CommentsForm} from '../../components/Categories';
+import { getPosts, getPostDetails } from '../../services';
+
+const PostDetails = () => {
+    return (
+        <div>
+            PostDetails
+        </div>
+    )
+}
+
+export default PostDetails
